2c769afef6cc37a100553d30217a3f377e23d55e
klondike <kprojection@gmail.com>
2010-02-08 03:02:56 -0500

    Added some index and a more informative debug output

--- articling.rb
+++ articling.rb
@@ -5,7 +5,9 @@ require 'sinatra'
 require 'mongo_mapper'
 
 get '/' do
-  Article.all.map(&:title).join '\n<br/>'
+  Article.all(:order => "created_at ASC").map do |article|
+    "#{article.title}:\n<br/>#{article.body}"
+  end.join "\n\n<br/><br/>"
 end
 
 
@@ -13,6 +15,11 @@ class Article
   include MongoMapper::Document
   
   key :slug, String, :required => true, :index => true
+  
+  ensure_index :tags
+  ensure_index :source
+  ensure_index :private
+  ensure_index :imported_at
   
   timestamps!
 end
