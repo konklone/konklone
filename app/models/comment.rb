6b1cdb7d9b5ee30a239370e3cfa80490ca97fee8
Eric Mill <konklone@gmail.com>
2012-12-31 20:14:46 -0500

    1.9

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,9 +10,9 @@ class Comment
   field :author_url
   field :body
   field :ip
-  field :hidden, :type => Boolean, :default => false
-  field :flagged, :type => Boolean, :default => false
-  field :mine, :type => Boolean, :default => false
+  field :hidden, type: Boolean, :default => false
+  field :flagged, type: Boolean, :default => false
+  field :mine, type: Boolean, :default => false
   
   index :author
   index :author_url
@@ -25,8 +25,8 @@ class Comment
   validates_presence_of :body
   validates_presence_of :author
   
-  scope :visible, :where => {:hidden => false, :flagged => false}
-  scope :flagged, :where => {:flagged => true}
+  scope :visible, where: {hidden: false, flagged: false}
+  scope :flagged, where: {flagged: true}
   
   
   # prefix URLs with http:// if they exist and don't have it
@@ -44,7 +44,7 @@ class Comment
   # not saved to db
   attr_accessor :user_agent, :referrer
   
-  rakismet_attrs :author_email => nil,
-      :comment_type => "comment", :content => :body,
-      :permalink => nil, :user_ip => :ip
+  rakismet_attrs author_email: nil,
+    comment_type: "comment", content: :body,
+    permalink: nil, user_ip: :ip
 end
