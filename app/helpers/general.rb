74622266dc024b00d2d938aa477c9a77813e4d4d
Eric Mill <konklone@gmail.com>
2013-05-27 00:15:24 -0400

    refactored/redesigned header links

--- app/helpers/general.rb
+++ app/helpers/general.rb
@@ -7,7 +7,7 @@ module Helpers
   module General
 
     def header_link(text, path)
-      "<a href=\"#{path}\" class=\"#{request.path == path ? "active" : ""}\">#{text}</a>"
+      "<a href=\"#{path}\" class=\"#{request.path.start_with?(path) ? "active" : ""}\">#{text}</a>"
     end
 
     # don't give me empty strings
@@ -15,58 +15,58 @@ module Helpers
       content = yield_content symbol
 
       # not sure why yield_content returns US-ASCII
-      content.force_encoding("UTF-8") if content 
-      
+      content.force_encoding("UTF-8") if content
+
       if content.present?
         content
       else
         nil
       end
     end
-    
+
     def get_ip
       forwarded = request.env['HTTP_X_FORWARDED_FOR']
       forwarded.present? ? forwarded.split(',').first : nil
     end
-    
+
     def paginate(per_page, criteria)
       page = (params[:page]).to_i || 1
       page = 1 if page < 1
-      
+
       [
         criteria.skip((page-1) * per_page).limit(per_page).to_a,
         page
       ]
     end
-    
+
     def previous_page?(documents, page, per_page)
       page > 1
     end
-    
+
     def next_page?(documents, page, per_page)
       documents.size == per_page
     end
-    
+
     def post_path(post)
       "/post/#{post.slug}"
     end
-    
+
     def comment_path(post)
       "/post/#{post.slug}/comments"
     end
-    
+
     def h(text)
       Rack::Utils.escape_html text
     end
-    
+
     def short_datetime(time)
       time.strftime "%Y-%m-%d"
     end
-    
+
     def long_datetime(time)
       time.xmlschema
     end
-    
+
     def short_date(time)
       if Time.now.year == time.year
         time.strftime "%b #{time.day}"
@@ -74,15 +74,15 @@ module Helpers
         time.strftime "%b #{time.day}, %Y"
       end
     end
-    
+
     def long_date(time)
       time.strftime "%B #{time.day}, %Y" # remove 0-prefix
     end
-    
+
     def rss_date(time)
       time.strftime "%a, %d %b %Y %H:%M:%S %T"
     end
-    
+
     def comment_time(time)
       # Aug 21, 12:03pm
       meridian = time.strftime "%p"
@@ -102,12 +102,12 @@ module Helpers
 
       # hack: make standalone img tags stand alone
       text.gsub!(/<p>(<img [^>]+>)<\/p>/) do
-        "<div class=\"container\">#{$1}</div>" 
+        "<div class=\"container\">#{$1}</div>"
       end
 
       # even standalone img tags with links around them
       text.gsub!(/<p>(<a [^>]+>?<img [^>]+><\/a>)<\/p>/) do
-        "<div class=\"container\">#{$1}</div>" 
+        "<div class=\"container\">#{$1}</div>"
       end
 
       text
@@ -120,8 +120,8 @@ module Helpers
 
       markdowned = Redcarpet::Markdown.new(
         renderer,
-        # no_intra_emphasis: true, 
-        autolink: true, 
+        # no_intra_emphasis: true,
+        autolink: true,
         space_after_headers: true
       )
 
@@ -164,20 +164,20 @@ module Helpers
       markdown = Redcarpet::Markdown.new(
         renderer,
         no_intra_emphasis: true,
-        autolink: true, 
+        autolink: true,
         space_after_headers: true
       )
 
       markdown.render body
     end
-    
+
     def url_escape(url)
       URI.escape url
     end
 
     def strip_tags(string)
       doc = Nokogiri::HTML string
-      (doc/"//*/text()").map do |text| 
+      (doc/"//*/text()").map do |text|
         text.inner_text.strip
       end.select {|text| text.present?}.join " "
     end
@@ -186,6 +186,6 @@ module Helpers
       return nil unless string
       Loofah.scrub_fragment(string, :prune).to_s.strip
     end
-    
+
   end
 end
