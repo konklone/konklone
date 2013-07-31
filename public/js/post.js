78a3257e8a1cebd1096341aef1445590c27b992e
Eric Mill <eric@konklone.com>
2013-07-31 12:07:23 -0400

    remove 'go to top' stuff

--- public/js/post.js
+++ public/js/post.js
@@ -1,42 +1,46 @@
-$("header.main .inner.article a.right").click(function() {
+$("header.main .inner h1 a").click(function() {
   window.location.hash = "";
   window.scrollTo(0,0);
   return false;
 });
 
-$("header.main .inner.me a.left").show();
+// $("header.main .inner.me a.left").show();
 
-// dynamically prepend an anchor link to every h2
-$("article .body h2").prepend("<span class=\"anchor\"></span><a class=\"block\"></a>");
-$("a.block").each(function(i, elem) {
-  var id = this.parentNode.id;
-  this.parentNode.id = null;
-  $(this).prev(".anchor")[0].id = id;
-  $(this).attr("href", "#" + id);
-});
 
 $(function() {
-  var articleHeader = $('article header');
 
-  var start = $(articleHeader).offset().top;
+  // replace blog header links with article title
+  var start = $("article header").offset().top;
   var offset = 0;
 
-  var headerMe = $("header.main .inner.me");
-  var headerArticle = $("header.main .inner.article");
-  headerArticle.find("h1").text($("article header .title").text());
+  var headerLinks = $("header.main .inner ul");
+  var headerTitle = $("header.main .inner h1");
+
+  var actualTitle = $("article header .title").text();
+  headerTitle.find("a").text(actualTitle);
 
   $.event.add(window, "scroll", function() {
     var p = $(window).scrollTop();
     var flip = p > (start - offset);
 
-    headerMe.css('display', flip ? 'none' : 'block');
-    headerArticle.css('display', flip ? 'block' : 'none');
+    headerLinks.css('display', flip ? 'none' : 'block');
+    headerTitle.css('display', flip ? 'block' : 'none');
   });
+
 
   // easy footnotes
   $("article small").each(function(i) {
     var note = "<sup>" + (i+1) + "</sup>";
     $(this).before(note).prepend(note);
+  });
+
+  // dynamically prepend an anchor link to every h2
+  $("article .body h2").prepend("<span class=\"anchor\"></span><a class=\"block\"></a>");
+  $("a.block").each(function(i, elem) {
+    var id = this.parentNode.id;
+    this.parentNode.id = null;
+    $(this).prev(".anchor")[0].id = id;
+    $(this).attr("href", "#" + id);
   });
 
 });
