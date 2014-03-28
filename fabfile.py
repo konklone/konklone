627c80a668b34a6209dfc609acae9e1503141712
Eric Mill <eric@konklone.com>
2014-03-28 17:24:11 -0400

    clean up old releases

--- fabfile.py
+++ fabfile.py
@@ -9,14 +9,25 @@ env.hosts = ["konklone.com"]
 branch = "master"
 repo = "git@github.com:konklone/konklone.git"
 
+keep = 5
+
 username = "konklone"
 home = "/home/konklone/konklone"
 shared_path = "%s/shared" % home
-version_path = "%s/versions/%s" % (home, time.strftime("%Y%m%d%H%M%S"))
+versions_path = "%s/versions" % home
+version_path = "%s/%s" % (versions_path, time.strftime("%Y%m%d%H%M%S"))
 current_path = "%s/current" % home
 
 
 # can be run only as part of deploy
+
+def cleanup():
+  versions = run("ls -x %s" % versions_path).split()
+  destroy = versions[:-keep]
+
+  for version in destroy:
+    command = "rm -rf %s/%s" % (versions_path, version)
+    run(command)
 
 def checkout():
   run('git clone -q -b %s %s %s' % (branch, repo, version_path))
@@ -38,9 +49,6 @@ def make_current():
 
 def set_crontab():
   run("cd %s && bundle exec rake set_crontab environment=%s current_path=%s" % (current_path, environment, current_path))
-
-def prune_releases():
-  pass
 
 
 ## can be run on their own
@@ -64,6 +72,7 @@ def deploy():
   execute(set_crontab)
   execute(stop)
   execute(start)
+  execute(cleanup)
   # execute(restart)
 
 # only difference is it uses start instead of restart
