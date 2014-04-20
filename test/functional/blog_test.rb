0a563d2ff85cf6a7e1c9cff091aa1f488d139bf3
Eric Mill <eric@konklone.com>
2014-04-20 17:21:59 -0400

    a few other sanity tests

--- test/functional/blog_test.rb
+++ test/functional/blog_test.rb
@@ -16,4 +16,29 @@ class BlogTest < Test::Unit::TestCase
     assert_response 200
   end
 
+  def test_visiting_a_draft_post
+    post = create :post, draft: true, private: false
+    get "/post/#{post.slug}"
+    assert_response 404
+  end
+
+  def test_visiting_a_private_post
+    post = create :post, draft: false, private: true
+    get "/post/#{post.slug}"
+    assert_response 404
+  end
+
+  def test_visiting_admin
+    get "/admin"
+    assert_response 200
+  end
+
+  def test_visiting_admin_post
+    post = create :published_post
+    get "/admin/post/#{post.slug}"
+    assert_response 404
+  end
+
+  # TODO: ensure admin area is accessible to admin
+
 end
